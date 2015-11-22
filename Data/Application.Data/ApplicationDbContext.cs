8684495f557f32b4f82c4459e94176aaaa177e4f
Bozhin Karaivanov <bozhink@gmail.com>
2015-11-22 15:03:21 +0200

    DatabaseConfig added

--- Data/Application.Data/ApplicationDbContext.cs
+++ Data/Application.Data/ApplicationDbContext.cs
@@ -3,12 +3,22 @@
     using Models;
     using Repositories;
     using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Data.Entity;
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>, IDbContext
     {
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            ////modelBuilder
+            ////    .Entity<ApplicationUser>()
+            ////    .HasMany
+
+            base.OnModelCreating(modelBuilder);
         }
 
         public static ApplicationDbContext Create()
---
+++ Web/Application.Web.Api/App_Start/DatabaseConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.Web.Api
+{
+    using Data;
+    using Data.Migrations;
+    using System.Data.Entity;
+
+    public static class DatabaseConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
+        }
+    }
+}
